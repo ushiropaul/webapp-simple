0d874a56e63b3d42edb5953b9dfa9ce5e5e85bd1
Paul Ushiro <ushiropaul2007@gmail.com>
2025-05-23 21:39:51 -0300

    4

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const supabaseUrl = 'https://tbdsqdyvxueryctivajr.supabase.co'; // Reemplazar
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiZHNxZHl2eHVlcnljdGl2YWpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMzc0MTIsImV4cCI6MjA2MzYxMzQxMn0.k_yFkt2VAEj6WyfiwPFrwF59IFJDzFwFA_P33KgSsuA'; // Reemplazar
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const { createClient } = supabase;
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
 
 async function cargarAlumnos() {
   const { data, error } = await supabase.from('alumnos').select('*');
